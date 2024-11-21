f090587e9bbd8b535a9aa070a249c8c927b43156
frrst-ian <frrstxd@proton.me>
2024-11-21 18:03:01 +0800

    Add helper functions to get parent and child index

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,19 @@ struct User {
   User(const string &name = "", const string &userRank, int userPower = 0)
       : username(name), rank(userRank), power(userPower) {}
 };
+
+class LeaderboardHeap {
+    private:
+        vector<User> heap;
+        bool isMaxHeap;
+
+        int parent(int i){ return (i-1)/ 2;}
+        int leftChild(int i ) { return 2 * i + 1;}
+        int rightChild(int i) { return 2 * i + 2;}
+
+        bool compare (const User &a , const User& b){
+            return isMaxHeap ? ( a.power > b.power) : (a.power < b.power);
+        }
+
+
+};
